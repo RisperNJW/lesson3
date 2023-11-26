0de8d72a3c4c9f1feaff03a83e3a8bbb4ba5da23
RisperNJW <150463973+RisperNJW@users.noreply.github.com>
2023-11-26 10:11:09 +0300

    Add files via upload

---
+++ Lesson3/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    const element = document.getElementById("text");
+    const paragraph = document.getElementById("paragraph");
+
+    element.innerHTML ="This text has been changed";
+    paragraph.innerHTML ="How do you like udemy!";
+}
